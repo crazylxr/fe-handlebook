923333a23256b9f3d97b0ad82aa26b3c3fa8467b
licai <licai@mogujie.com>
2020-04-09 23:31:18 +0800

    ..

---
+++ .vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // 站点配置
+  plugins: [
+    [
+      'vuepress-plugin-yuque', {
+	      authToken: 'a6zlL3YXRrpce4ndXS6UzDjgpW9QfdZAFpvagxsI',
+	      repoUrl: 'https://www.yuque.com/xiaoran/excellentarticles',
+	      html: true,
+      }
+    ]
+  ]
+}
